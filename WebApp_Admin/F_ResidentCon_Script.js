ba09ac92aff5480aae8ab6b3d948645501d90307
CJ Fallorina <chescafallorina0@gmail.com>
2024-10-06 04:22:48 -0700

    modified:   WebApp_Admin/F_RecordsCon_Style.css 	modified:   WebApp_Admin/F_Records_Res.html 	modified:   WebApp_Admin/F_ResidentCon_Script.js

--- WebApp_Admin/F_ResidentCon_Script.js
+++ WebApp_Admin/F_ResidentCon_Script.js
@@ -1,10 +1,41 @@
 function updateLabel() {
     const toggle = document.getElementById('resToggle');
-    const label = document.querySelector('.rToggleLbl');
+    const label = document.querybldgselector('.rToggleLbl');
 
     if (toggle.checked) {
-        label.textContent = 'Former'; // Update to Former Residents
+        label.textContent = 'Former'; 
     } else {
-        label.textContent = 'Current'; // Update to Current Residents
+        label.textContent = 'Current'; 
     }
 }
+
+const bldgselect = document.getElementById("bldgSelect");
+
+const bldgallOption = document.createElement("option");
+bldgallOption.value = "all";
+bldgallOption.textContent = "All";
+bldgselect.appendChild(bldgallOption); 
+
+for (let i = 1; i <= 30; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = `Bldg ${i}`; 
+    bldgselect.appendChild(option);
+}
+
+const floorselect = document.getElementById("floorSelect");
+
+const flrallOption = document.createElement("option");
+flrallOption.value = "all";
+flrallOption.textContent = "All";
+floorselect.appendChild(flrallOption); 
+
+// Create and append floor numbers as "Floor 1-5"
+for (let i = 1; i <= 5; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = `Floor ${i}`;  
+    floorselect.appendChild(option);
+}
+
+
